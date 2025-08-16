46d27247426f7e27205f7eb6ca9e2ccfd1b40e77
Mohammed Sohail <Mohammedsohail2603@gmail.com>
2025-08-16 13:43:42 +0530

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all code as newer version of js
+
+
+// alert(3+3) // we are using nodejs, not a browser
+
+console.log(3 
+    +
+    3) // readability is top priority
+
+let name ="Sohail"
+let age = 26
+let isLoggedIn = false
+let state;
+
+// number => 2 power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null =>standalone object
+// undefined => 
+// symbol => unique
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
