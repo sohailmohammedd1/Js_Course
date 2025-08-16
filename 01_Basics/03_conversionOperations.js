11465e825bbefc3b777c865ecb49e212f5f78b7a
Mohammed Sohail <Mohammedsohail2603@gmail.com>
2025-08-16 15:45:05 +0530

    conversion and operations

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "33kfk";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33kfk" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "sohail"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof isLoggedIn)
+
+// 1 => true: 0 => false
+// "" => false 
+// "sohail" => true
