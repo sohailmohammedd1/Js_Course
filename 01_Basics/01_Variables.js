8c83faae15123ef0e287e2888aa87e3390e275be
Mohammed Sohail <Mohammedsohail2603@gmail.com>
2025-08-16 13:35:35 +0530

    add Varables.js

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 171055
+
+let accountName = "sohail"
+let accountEmail = "sohail@google.com"
+let accoutnPassword = "6u7y8i9io"
+let accountCity = "Hyderabad"
+let accountState;
+
+// do not use var because of issues in block and functional scopes
+
+// accountId = 16113 => cannot change const
+
+console.table([accountId,accountName,accountEmail,accoutnPassword,accountCity,accountState])
+
