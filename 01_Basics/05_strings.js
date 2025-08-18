aba13440ece5b44114f7cb04250c3993e685fb98
Mohammed Sohail <Mohammedsohail2603@gmail.com>
2025-08-18 13:59:16 +0530

    string practice

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -4,20 +4,19 @@ const repoCount = 1
 
 // console.log(name + repoCount + " value");// old style
 
+// template literals (modern style) 
 console.log(`hello i'm ${name} and my repo value is ${repoCount}`);
 
+// String objects and properties
 const fullName =new String ("Mohammed Sohail");
-
 console.log(fullName.__proto__);
 console.log(fullName.length);
-
 console.log(fullName.charAt('0'));
-
 console.log(fullName[5]);
 console.log(fullName.indexOf('d',4));
 
+// String methods
 const newName = fullName.toUpperCase();
-
 console.log(newName);
 
 const firstName = fullName.substring(0,8);
@@ -27,29 +26,22 @@ const lastName = fullName.substring(9);
 console.log(lastName);
 
 console.log(fullName.trim());
-
 console.log(fullName.slice(-8,16)); // nth index - negative index
 
+// Url manipulation
 const url1 = "https:://sohail@google.com/sohail%20mohammed";
-
 console.log(url1.replace('%20','.'));
-
-
 console.log(url1.includes('sohail'));
 
 
 
 
-
+// String splitting
 const intro1 = "my name is walter hartwell white";
-
 const words = intro1.split(" ");
 console.log(words.length);
-
 console.log(words[3])
-
 console.log(intro1.split(""));
-
 console.log(intro1.length);
 
 const strCopy =intro1.split();
